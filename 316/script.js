f8eaf993b888cd47bdeb9ebcb45306a50eca7bae
Amblar-h00f <amberkollm3@gmail.com>
2025-03-25 15:08:08 -0600

    R-ALAB

---
+++ 316/script.js
@@ -0,0 +1,10 @@
+
+
+const mainEl = document.querySelector('main');
+
+mainEl.style.backgroundColor = 'var(--sub-menu-bg)';
+
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>' , 
+
+mainEl.element.classList.add('flex-ctr', 'another-class');
+
