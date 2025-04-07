de53f0c33e2b8ce30244f6780bd187c75119bef5
Amblar-h00f <amberkollm3@gmail.com>
2025-04-07 14:59:14 -0600

    trying to clear this out

---
+++ warmup.js
@@ -0,0 +1,42 @@
+// Functions Calculator
+/*
+ * 1.) Declare a function named calculate
+ * 2.) Function takes 3 parameters: num1, num2, operand
+ * 3.) Within function use a switch statement that observes the operand
+ * 4.) The switch statement has 4 'cases':
+ ****** Addition, Subtraction, Multiplication, Division
+ * 5.) Each case will call (invoke/run) a helper function that returns the output from each of those functions
+ *
+ *  - 5 Total functions - calculate, sum, difference, multiply, divide
+ *  - Bonus:
+ *  - Use rest parameters or the 'arguments' keyword to make your function more dynamic.
+ *  - Add exponentiation to your calculator
+ */
+function calculate (num1, num2, num3, divide,) {
+    switch(parameter) {
+        case num1:
+            
+        break;
+        case num2:
+
+        break;
+        case num3:
+
+        break;
+        case divide:
+
+
+            break;
+            
+
+        default:
+
+
+    }
+}
+
+function calculate (num1, num2, num3,) {
+    switch (operand){
+        case '+' :                                 
+    }
+}
